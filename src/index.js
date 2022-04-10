1d2516f3fd8f299231feef64bcca3ec822dd1750
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-04-09 23:31:32 -0400

    Agregando las imagenes desde javascript, ademas de crear la funcion para generar el numero random de imagen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const URL = "https://randomfox.ca";
+const app = document.querySelector('#images');
 
-console.log('Happy hacking :)')
+const random = (min,max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const createImageNode = () => {
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'p-4';
+  
+  const image = document.createElement('img');
+  image.className = 'mx-auto';
+  image.width = '320';
+  image.src = `${URL}/images/${random(1,122)}.jpg`;
+
+  imageContainer.append(image);
+
+  return imageContainer;
+}
+
+const addImage = () => {
+  const newImage = createImageNode();
+  app.appendChild(newImage);
+}
+
+const addButton = document.querySelector('#addImage');
+addButton.addEventListener('click', addImage);
